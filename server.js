e432b8dd5dbec294456b7a6f93cffb9859f6dee0
Michael Timmons <michael.timmons@protonmail.com>
2022-12-09 16:57:03 -0500

    Add socket connection

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
