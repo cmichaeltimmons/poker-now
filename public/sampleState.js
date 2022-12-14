702677ec0148672850e2917ff5a8644702646e4f
Michael Timmons <michael.timmons@protonmail.com>
2022-12-13 22:50:28 -0500

    Add sample state

---
+++ public/sampleState.js
@@ -0,0 +1,96 @@
+// this the sample state for the ui to render
+const sampleState = {
+    "table": {
+        "players": [
+            {
+                "name": "Austin",
+                "n_chips": 900,
+                "cards": [
+                    {
+                        "rank": "3",
+                        "suit": "clubs"
+                    },
+                    {
+                        "rank": "queen",
+                        "suit": "hearts"
+                    }
+                ],
+                "is_active": false,
+                "id": 118884741763112942970546126670580930208,
+                "order": 1,
+                "is_small_blind": false,
+                "is_big_blind": false,
+                "is_dealer": false
+            },
+            {
+                "name": "Ben",
+                "n_chips": 950,
+                "cards": [
+                    {
+                        "rank": "4",
+                        "suit": "diamonds"
+                    },
+                    {
+                        "rank": "10",
+                        "suit": "diamonds"
+                    }
+                ],
+                "is_active": false,
+                "id": 330165848609449054910290454928272305771,
+                "order": 0,
+                "is_small_blind": false,
+                "is_big_blind": false,
+                "is_dealer": false
+            }
+        ],
+        "pot": {
+            "uid": "53f9ff4e94d04a73bfaffceacad75cb6",
+            "total": 0,
+            "side_pots": []
+        },
+        "community_cards": [
+            {
+                "rank": "king",
+                "suit": "spades"
+            },
+            {
+                "rank": "4",
+                "suit": "spades"
+            },
+            {
+                "rank": "2",
+                "suit": "diamonds"
+            },
+            {
+                "rank": "7",
+                "suit": "diamonds"
+            },
+            {
+                "rank": "ace",
+                "suit": "hearts"
+            }
+        ],
+        "n_games": 0
+    },
+    "player": {
+        "name": "Austin",
+        "n_chips": 900,
+        "cards": [
+            {
+                "rank": "3",
+                "suit": "clubs"
+            },
+            {
+                "rank": "queen",
+                "suit": "hearts"
+            }
+        ],
+        "is_active": false,
+        "id": 118884741763112942970546126670580930208,
+        "order": 1,
+        "is_small_blind": false,
+        "is_big_blind": false,
+        "is_dealer": false
+    },
+    "action": "call"
+}
